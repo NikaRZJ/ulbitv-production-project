eff489b1eb9e261e49212e17181e0e091505f745
Artyukh Nikita <n.artyukh@brooma.ru>
2025-06-01 15:33:32 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
+import './styles/index.scss';
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
+
+const App = () => {
+    const { theme, toggleTheme } = useTheme();
+    // const bool = true;
+
+    return (
+        <div className={classNames('app', { hovered: true, selected: false}, [theme, 'cls2', 'cls3'])}> 
+            <button onClick={toggleTheme}>TOGGLE THEME</button>
+            <Link to={'/'}>Главная</Link> 
+            <Link to={'/about'}>О сайте</Link>
+           
+            <Suspense fallback={<div>Loading...</div>}>
+               <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                </Routes>        
+            </Suspense>
+        </div>
+    ); 
+};
+
+export default App;
