e595abf534ce939aba6e4948b161d3b0b8bb2fb4
Artyukh Nikita <n.artyukh@brooma.ru>
2025-06-01 17:14:55 +0700

    [2.1] App router, config for router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import { useTheme } from "./providers/ThemeProvider";
 import './styles/index.scss';
 import { AboutPage } from "pages/AboutPage";
 import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -14,13 +15,7 @@ const App = () => {
             <button onClick={toggleTheme}>TOGGLE THEME</button>
             <Link to={'/'}>Главная</Link> 
             <Link to={'/about'}>О сайте</Link>
-           
-            <Suspense fallback={<div>Loading...</div>}>
-               <Routes>
-                    <Route path={'/about'} element={<AboutPage />}/>
-                    <Route path={'/'} element={<MainPage />}/>
-                </Routes>        
-            </Suspense>
+            <AppRouter />
         </div>
     ); 
 };
