0681be15ed7a7a23ce838c57b6f57bf2015a6547
Artyukh Nikita <n.artyukh@brooma.ru>
2025-07-19 16:57:38 +0700

    [20] feat: add jest, add first test

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,7 +4,9 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
     return [
         cls,
         ...additional.filter(Boolean),
-        Object.entries(mods).filter(([classNames, value]) => Boolean(value))
-            .map(([classNames]) => classNames),
-    ].join(' ');
+        ...Object.entries(mods)
+            .filter(([, value]) => Boolean(value))
+            .map(([className]) => className),
+    ]
+        .join(' ');
 }
