8cf4a1b9bc03160f8406ff2238b479950139ebfe
Artyukh Nikita <n.artyukh@brooma.ru>
2025-07-19 17:00:38 +0700

    fix: eslint rule max-len for comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'react/button-has-type': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': ['error', { code: 100, ignoreComments: true }],
     },
     globals: {
         __IS_DEV__: true,
