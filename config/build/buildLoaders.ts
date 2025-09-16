6a637d68f9590d9c4fe10c328756686ba8d197a9
Artyukh Nikita <n.artyukh@brooma.ru>
2025-09-16 18:31:21 +0700

    [25] wip: video timing 18:00

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { RuleSetRule } from 'webpack';
 import { BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
 
 export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
     const fileLoader = {
@@ -17,25 +17,7 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
         use: ['@svgr/webpack'],
     };
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            // Creates `style` nodes from JS
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath: string) => resPath.includes('.module.scss'),
-                        localIdentName: isDev ? '[path][name]__[local]--[hash:base64:8]' : '[hash:base64:8]',
-                    },
-                },
-            },
-            // Compiles Sass to CSS
-            'sass-loader',
-        ],
-    };
+    const cssLoader = buildCssLoader(isDev);
 
     const typescriptLoader = {
         test: /\.tsx?$/,
---
+++ config/storybook/webpack.config.ts
@@ -0,0 +1,19 @@
+import { Configuration } from "webpack";
+import { BuildPaths } from "../build/types/config";
+import path from "path";
+import { buildCssLoader } from "../build/loaders/buildCssLoader";
+
+export default ({ config }: {config: Configuration}) => {
+    const paths: BuildPaths = {
+        build: '',
+        entry: '',
+        html: '',
+        src: path.resolve(__dirname, '..', '..', 'src'),
+    }
+    config.resolve.modules.push(paths.src);
+    config.resolve.extensions.push('.ts', '.tsx');
+
+    config.module.rules.push(buildCssLoader(true));
+    
+    return config;
+}
